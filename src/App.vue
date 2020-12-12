498caad488ada349510409682f2de7808a7f04f5
yh <yuehong_web@163.com>
2020-12-12 11:56:01 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="layui-container">
+    <form class="layui-form layui-form-pane" action="">
+      <div class="layui-form-item">
+        <label class="layui-form-label">手机/邮箱</label>
+        <div class="layui-input-block">
+          <input
+            type="text"
+            name="title"
+            required
+            lay-verify="required"
+            placeholder="请输入手机/邮箱"
+            autocomplete="off"
+            class="layui-input"
+          />
+        </div>
+      </div>
+      <div class="layui-form-item">
+        <label class="layui-form-label">密码</label>
+        <div class="layui-input-block">
+          <input
+            type="password"
+            name="title"
+            required
+            lay-verify="required"
+            placeholder="请输入密码"
+            autocomplete="off"
+            class="layui-input"
+          />
+        </div>
+      </div>
+      <div class="layui-form-item">
+        <label class="layui-form-label">验证码</label>
+        <div class="layui-input-block">
+          <input
+            type="text"
+            name="title"
+            required
+            lay-verify="required"
+            placeholder="请输入验证码"
+            autocomplete="off"
+            class="layui-input"
+          />
+        </div>
+      </div>
+      <button type="button" class="layui-btn">立即登录</button>
+<a href="http://www.layui.com" class="text-btn">忘记密码?</a>
+    </form>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#app{
+  background-color: #f2f2f2;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.layui-container{
+  background-color: #fff;
+}
+.layui-input-block{
+  width: 200px !important;
+}
+.text-btn{
+  margin-left: 24px;
+  &:hover{
+    color: #01AAED;
   }
 }
+
 </style>
