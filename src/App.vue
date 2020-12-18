db2aedcd3ff6e4291cc0f80ed65dba6c5b07ef50
yuehong <yuehong_web@163.com>
2020-12-18 18:01:49 +0800

    登录、注册、忘记密码模块UI

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,26 @@
 <template>
   <div id="app">
-    <div class="layui-container">
-    <form class="layui-form layui-form-pane" action="">
-      <div class="layui-form-item">
-        <label class="layui-form-label">手机/邮箱</label>
-        <div class="layui-input-block">
-          <input
-            type="text"
-            name="title"
-            required
-            lay-verify="required"
-            placeholder="请输入手机/邮箱"
-            autocomplete="off"
-            class="layui-input"
-          />
-        </div>
-      </div>
-      <div class="layui-form-item">
-        <label class="layui-form-label">密码</label>
-        <div class="layui-input-block">
-          <input
-            type="password"
-            name="title"
-            required
-            lay-verify="required"
-            placeholder="请输入密码"
-            autocomplete="off"
-            class="layui-input"
-          />
-        </div>
-      </div>
-      <div class="layui-form-item">
-        <label class="layui-form-label">验证码</label>
-        <div class="layui-input-block">
-          <input
-            type="text"
-            name="title"
-            required
-            lay-verify="required"
-            placeholder="请输入验证码"
-            autocomplete="off"
-            class="layui-input"
-          />
-        </div>
-      </div>
-      <button type="button" class="layui-btn">立即登录</button>
-<a href="http://www.layui.com" class="text-btn">忘记密码?</a>
-    </form>
-    </div>
+    <Header />
+    <router-view></router-view>
+    <Footer />
   </div>
 </template>
-
-<style lang="scss">
-#app{
-  background-color: #f2f2f2;
-}
-.layui-container{
-  background-color: #fff;
-}
-.layui-input-block{
-  width: 200px !important;
-}
-.text-btn{
-  margin-left: 24px;
-  &:hover{
-    color: #01AAED;
+<script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Footer
   }
+
 }
+</script>
+<style lang="scss">
+// 这里是全局的样式
+@import "assets/layui/css/layui.css";
+@import "assets/css/global.css";
+@import "assets/layui/css/modules/layer/default/layer.css";
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
+const Login = () => import(/* webpackChunkName: "login" */ '@/views/Login.vue')
+const Register = () => import(/* webpackChunkName: "register" */ '@/views/Register.vue')
+const Forget = () => import(/* webpackChunkName: "forget" */ '@/views/Forget.vue')
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/forget',
+    name: 'forget',
+    component: Forget
   }
 ]
 
